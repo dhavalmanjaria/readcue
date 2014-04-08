77bbde465070a1fbb336a3e3118f51df0379a91c
Darcy Brás da Silva <dardevelin@cidadecool.com>
2014-04-08 03:13:33 +0100

    first major improvements

--- readcue.py
+++ readcue.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
 
-##   Readcue.py
-##	 
-##	 This script reads the cue file from a folder, reads the name of the
-##   FLAC file from that and separates it into it's individial songs.
-
+#############################################################################
+## readcue.py
+## This file is part of readcue
+## Copyright (C) 2014 Dhaval Anjaria
+## Copyright (C) 2014 Darcy Bras da Silva
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/
+#############################################################################
+## this script reads the cue file from a folder, reads the name of the
+## flac file and from that separates it into it's individual songs
+#############################################################################
 
 import os, subprocess
 
